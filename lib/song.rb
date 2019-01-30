7adb4cb67fd78a173035b51e95f14f1acbc4785f
Emily Wagner <emilywagner598@gmail.com>
2019-01-30 22:25:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,11 +46,7 @@ class Song
   end
 
   def self.alphabetical
-    names = []
-    @@all.each do |element|
-      names << element.name
-    end
-    names.sort
+    @@all.sort_by {|obj| obj.name}
   end
 
 
